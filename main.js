4c6436895233dbc19eb28de2c64be4eeb5b6e079
Natalie <natalie@MacBook-Pro.local>
2020-06-13 14:13:22 +0200

    bla

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-const baseURL = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
+const baseURLRandom = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
+const baseURLKeyword = "https://quote-garden.herokuapp.com/api/v2/quotes/:searchQuery?page=1&limit=10"
 
 const searchRandom = document.querySelector("#searchRandom");
-const ausgabe = document.querySelector("#ausgabe");
+const searchByKeyword = document.querySelector("#searchByKeyword");
+const ausgabeText = document.querySelector("#ausgabeText");
+const ausgabeAuthor = document.querySelector("#ausgabeAuthor");
 
 searchRandom.addEventListener("click", e => {
-    fetch(baseURL)
+    fetch(baseURLRandom)
     .then(respond => respond.json())
     .then(result => {
-        console.log(result.quote.quoteText)
-        ausgabe.innerHTML = result.quote.quoteText;
+        console.log(result.quote);
+        ausgabeText.innerHTML = result.quote.quoteText;
+        ausgabeAuthor.innerHTML = result.quote.quoteAuthor;
+
     });
     })
+
+    // searchRandom.addEventListener("click", e => {
+    //     const q = document.getElementById("keyword").value;
+    //     fetch(`${baseURLKeyword}${q}`)
+    //     .then(respond => respond.json())
+    //     .then(result => {
+    //         console.log(result.quote.quoteText)
+    //         ausgabe.innerHTML = result.quote.quoteText;
+    //     });
+    //     })
+
+
+      
