df300024c215c5e4abcda3cc065bf78ebb310a2a
Natalie <natalie@MacBook-Pro.local>
2020-06-13 14:36:34 +0200

    bla

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const baseURL = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
+const baseURLRandom = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
 const searchRandom = document.querySelector("#searchRandom");
 const searchByKeyword = document.querySelector("#searchByKeyword");
 const ausgabeText = document.querySelector("#ausgabeText");
@@ -8,7 +8,7 @@ searchRandom.addEventListener("click", e => {
     fetch(baseURLRandom)
     .then(respond => respond.json())
     .then(result => {
-        console.log(result.quote);
+        console.log(result.quote.quoteText);
         ausgabeText.innerHTML = result.quote.quoteText;
         ausgabeAuthor.innerHTML = result.quote.quoteAuthor;
 
